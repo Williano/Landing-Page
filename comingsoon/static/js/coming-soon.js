1a924835b366de2bf40c305b13badbd863cd19f0
Williano <paawilly17@gmial.com>
2018-01-20 10:59:38 -1200

    Coming-soon.js

--- comingsoon/static/js/coming-soon.js
+++ comingsoon/static/js/coming-soon.js
@@ -3,8 +3,8 @@
 
   // Vide - Video Background Settings
   $('body').vide({
-    mp4: "mp4/bg.mp4",
-    poster: "img/bg-mobile-fallback.jpg"
+    mp4: "/static/mp4/bg.mp4",
+    poster: "/static/img/bg-mobile-fallback.jpg"
   }, {
     posterType: 'jpg'
   });
